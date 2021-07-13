e9fba907f7b741477045ac64cd50151c9c3a1b45
Neil Gibbons <66751817+Nyptop@users.noreply.github.com>
2021-07-13 12:50:04 +0100

    finished week 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,85 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// problems:
+
+// Why is it going straight to BLACK at the start when I haven't pressed a key
+
+// Why do I get the error at line 31: Desination is M but A = 24608 is an illegal memory address
+
+
+@SCREEN
+D=A
+@addr
+M=D
+
+//Setting Colour
+@KBD
+D=M
+
+@BLACK
+D;JEQ
+
+@WHITE
+D;JNE
+
+// ---------------
+
+(BLACK)
+@KBD
+D=A
+@addr
+D=D-M
+@END
+D;JLE
+
+
+@addr
+A=M
+M=-1
+
+@1
+D=A
+@addr
+M=D+M
+
+@BLACK
+D;JMP
+
+// -------------
+
+(WHITE)
+@KBD
+D=A
+@addr
+D=D-M
+@END
+D;JLE
+
+@addr
+A=M
+M=0
+
+@1
+D=A
+@addr
+M=D+M
+
+@WHITE
+D;JMP
+
+(END)
+@SCREEN
+D=A
+@addr
+M=D
+
+@KBD
+D=M
+
+@BLACK
+D;JEQ
+
+@WHITE
+D;JNE
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -1,12 +1,42 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
+@R0
+D=M
+@a
+M=D // a = R0
+@R1
+D=M
+@b
+M=D // b = R1
+@total
+M=0
 
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-//
-// This program only needs to handle arguments that satisfy
-// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+(LOOP)
+@b
+D = M
+@STOP
+D;JEQ
 
-// Put your code here.
+@b
+M=M-1
+@a
+D=M
+@total
+M=D+M
+
+@LOOP
+0;JMP
+
+(STOP)
+@total
+D=M
+@R2
+M=D
+
+(END)
+@END
+0;JMP
+
+
+
+
+
+
